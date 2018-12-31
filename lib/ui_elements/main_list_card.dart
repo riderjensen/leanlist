848add295f662cf075621ae1966912a6c0305645
Rider <riderjensen@gmail.com>
2018-12-31 16:09:48 -0700

    Card stage for number of cards

---
+++ lib/ui_elements/main_list_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainListCard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainListCard();
+  }
+}
+
+class _MainListCard extends State<MainListCard> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
