a37a2ed01ed3cf7ccddd098b32c81c05e5dd165f
Rider <riderjensen@gmail.com>
2018-12-31 20:56:33 -0700

    Dummy data

---
+++ lib/dummyData.dart
@@ -0,0 +1,62 @@
+final List ourList = [
+  {
+    'id': '1',
+    'title': 'My first list',
+    'creator': 'Rider',
+    'permissions': 'full',
+    'items': {
+      'incomplete': [
+        'Here is an undone item',
+        'here is the second undone item',
+        'here is the thrid undone item'
+      ],
+      'complete': [
+        {
+          'item': 'here is the first item',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item',
+          'date': 'date here',
+          'userCom': 'Rider'
+        }
+      ]
+    }
+  },
+  {
+    'id': '2',
+    'title': 'My second list',
+    'creator': 'Rider',
+    'permissions': 'full',
+    'items': {
+      'incomplete': [
+        'Here is an undone item on list two',
+        'here is the second undone item on list two',
+        'here is the thrid undone item on list two'
+      ],
+      'complete': [
+        {
+          'item': 'here is the first item on list two COMPLETE',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two COMPLETE',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two COMPLETE',
+          'date': 'date here',
+          'userCom': 'Rider'
+        }
+      ]
+    }
+  }
+];
