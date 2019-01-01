4bb5fa345b7b294209835b6924ddc1b97b51bc27
Rider <riderjensen@gmail.com>
2019-01-01 16:31:27 -0700

    Creating share icon with alert dialog

--- lib/pages/list_one_list.dart
+++ lib/pages/list_one_list.dart
@@ -9,14 +9,49 @@ class ListOneList extends StatelessWidget {
 
   ListOneList(this.listId);
 
+  Future<void> _createShareAlert(BuildContext context, String shareId) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Your Share ID'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(shareId),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Okay'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> ourItem = ourList[int.parse(listId)];
+    Map<dynamic, dynamic> ourItem = ourList[int.parse(listId)];
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text(ourItem['title']),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {
+                _createShareAlert(context, ourItem['shareId']);
+              },
+            )
+          ],
           bottom: TabBar(
             tabs: <Widget>[
               Tab(
