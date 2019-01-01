9bb8d51158c113048f4ac1da25d4ec3687e3c130
Rider <riderjensen@gmail.com>
2019-01-01 15:23:04 -0700

    Creating dialog for adding old list

---
+++ lib/form_elements/add_list_dialog.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class AddListDialog extends StatelessWidget {
+  final Map<String, String> _formData = {'code': null};
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter a code'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text('Enter the code you were given'),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    onSaved: (String value) {
+                      _formData['code'] = value;
+                    },
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'A code is required';
+                      }
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Add'),
+          onPressed: () {
+            // await finding the list and adding them to it
+            _formKey.currentState.save();
+            if (!_formKey.currentState.validate()) {
+              return;
+            }
+            print(_formData['code']);
+            // this push isnt working, need to basically refresh the home page
+            Navigator.pushReplacementNamed(context, '/');
+          },
+        ),
+      ],
+    );
+  }
+}
