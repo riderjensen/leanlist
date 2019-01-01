e794cba37ab29f198f4dd7d7d3f6a7ed6176d9c7
Rider <riderjensen@gmail.com>
2018-12-31 20:56:47 -0700

    Exporting dummyData

--- lib/pages/list_lists.dart
+++ lib/pages/list_lists.dart
@@ -1,41 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../ui_elements/main_list_card.dart';
+import '../dummyData.dart';
 
 class ListLists extends StatelessWidget {
-  final List ourList = [
-    {
-      'id': '1',
-      'title': 'My first list',
-      'creator': 'Rider',
-      'permissions': 'full',
-      'items': {
-        'incomplete': [
-          'Here is an undone item',
-          'here is the second undone item',
-          'here is the thrid undone item'
-        ],
-        'complete': [
-          {
-            'item': 'here is the first item',
-            'date': 'date here',
-            'userCom': 'Rider'
-          },
-          {
-            'item': 'here is the second item',
-            'date': 'date here',
-            'userCom': 'Rider'
-          },
-          {
-            'item': 'here is the third item',
-            'date': 'date here',
-            'userCom': 'Rider'
-          }
-        ]
-      }
-    }
-  ];
-
   Widget returnEmpty() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
