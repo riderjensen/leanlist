af9116861cef9a0531deeb589bbd317b1117d776
Rider <riderjensen@gmail.com>
2018-12-31 17:13:16 -0700

    Moving items into a list of maps

--- lib/pages/list_lists.dart
+++ lib/pages/list_lists.dart
@@ -1,51 +1,55 @@
 import 'package:flutter/material.dart';
 
+import '../ui_elements/main_list_card.dart';
+
 class ListLists extends StatelessWidget {
-  final Map<String, dynamic> ourList = {
-    'title': 'My first list',
-    'creator': 'Rider',
-    'permissions': 'full',
-    'items': {
-      'incomplete': [
-        'Here is an undone item',
-        'here is the second undone item',
-        'here is the thrid undone item'
-      ],
-      'complete': [
-        {
-          'item': 'here is the first item',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item',
-          'date': 'date here',
-          'userCom': 'Rider'
-        }
-      ]
+  final List ourList = [
+    {
+      'title': 'My first list',
+      'creator': 'Rider',
+      'permissions': 'full',
+      'items': {
+        'incomplete': [
+          'Here is an undone item',
+          'here is the second undone item',
+          'here is the thrid undone item'
+        ],
+        'complete': [
+          {
+            'item': 'here is the first item',
+            'date': 'date here',
+            'userCom': 'Rider'
+          },
+          {
+            'item': 'here is the second item',
+            'date': 'date here',
+            'userCom': 'Rider'
+          },
+          {
+            'item': 'here is the third item',
+            'date': 'date here',
+            'userCom': 'Rider'
+          }
+        ]
+      }
     }
-  };
+  ];
+
+  Widget returnEmpty() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 100,
+        ),
+        Text('No list currently created.'),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ListView(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 100,
-              ),
-              Text('No list currently created.'),
-            ],
-          )
-        ],
-      ),
-    );
+        child: ourList == null ? returnEmpty() : MainListCard(ourList));
   }
 }
