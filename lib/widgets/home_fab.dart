d793006e533a6a59f8529432dc9443f93b5bc1f0
Rider <riderjensen@gmail.com>
2019-01-01 15:23:58 -0700

    Adding new code for labeled fabs

--- lib/widgets/home_fab.dart
+++ lib/widgets/home_fab.dart
@@ -1,20 +1,19 @@
-import 'dart:math' as math;
-
+import 'package:flutter_fab_dialer/flutter_fab_dialer.dart';
 import 'package:flutter/material.dart';
 
+import '../form_elements/add_list_dialog.dart';
 
-class HomeFab extends StatefulWidget{
+class HomeFab extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return _HomeFab();
-    }
+  State<StatefulWidget> createState() {
+    return _HomeFab();
+  }
 }
 
-class _HomeFab extends State<HomeFab> with TickerProviderStateMixin{
-AnimationController _controller;
+class _HomeFab extends State<HomeFab> with TickerProviderStateMixin {
+  AnimationController _controller;
 
-
-@override
+  @override
   void initState() {
     _controller = AnimationController(
       vsync: this,
@@ -23,30 +22,54 @@ AnimationController _controller;
     super.initState();
   }
 
+  Future _addNewListToAccount() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AddListDialog();
+      },
+    );
+  }
+
+  void _sendToCreatePage() {
+    Navigator.of(context).pushNamed('/create');
+  }
+
   @override
-    Widget build(BuildContext context) {
-      return FloatingActionButton(
-        onPressed: () {
-          if (_controller.isDismissed) {
-                  _controller.forward();
-                } else {
-                  _controller.reverse();
-                }
-        },
-        tooltip: 'Add List',
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (BuildContext context, Widget child){
-            return Transform(
-              transform:
-                  Matrix4.rotationZ(_controller.value * 0.5 * math.pi),
-              alignment: FractionalOffset.center,
-              child: Icon(_controller.isDismissed
-                  ? Icons.add
-                  : Icons.close),
-            );
-          },
+  Widget build(BuildContext context) {
+    final List<FabMiniMenuItem> _fabMiniMenuItemList = [
+      new FabMiniMenuItem.withText(
+          new Icon(Icons.add),
+          Colors.blue,
+          4.0,
+          "Button menu",
+          _addNewListToAccount,
+          "Enter an old code",
+          Colors.blue,
+          Colors.white,
+          true),
+      new FabMiniMenuItem.withText(
+          new Icon(Icons.add),
+          Colors.blue,
+          4.0,
+          "Button menu",
+          _sendToCreatePage,
+          "Create a new list",
+          Colors.blue,
+          Colors.white,
+          true),
+    ];
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        new FabDialer(
+          _fabMiniMenuItemList,
+          Colors.blue,
+          new Icon(Icons.add),
         ),
-      );
-    }
+      ],
+    );
+  }
 }
