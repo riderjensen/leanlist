226f17c47daf722f4db88e33bac220aa3c33f2de
Rider <riderjensen@gmail.com>
2018-12-31 17:13:32 -0700

    Render a list view of lists

--- lib/ui_elements/main_list_card.dart
+++ lib/ui_elements/main_list_card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MainListCard extends StatefulWidget {
+  final List ourList;
+
+  MainListCard(this.ourList);
+
   @override
   State<StatefulWidget> createState() {
     return _MainListCard();
@@ -8,8 +12,43 @@ class MainListCard extends StatefulWidget {
 }
 
 class _MainListCard extends State<MainListCard> {
+  Widget returnCard(int) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.album),
+              title: Text(widget.ourList[int]['title']),
+              subtitle: Text('Creator: ${widget.ourList[int]['creator']}'),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('View'),
+                    onPressed: () {
+                      // send them to list view page
+                      print('You clicked the button');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return ListView.builder(
+      itemCount: widget.ourList.length,
+      itemBuilder: (context, int) {
+        return returnCard(int);
+      },
+    );
   }
 }
