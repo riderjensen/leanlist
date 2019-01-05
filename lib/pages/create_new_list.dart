da80fc7fca232ea60b1524b267ca1a189a37ab1e
Rider <riderjensen@gmail.com>
2019-01-05 11:30:35 -0700

    Combine all inputs into the create_new_list file

--- lib/pages/create_new_list.dart
+++ lib/pages/create_new_list.dart
@@ -1,25 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
-import '../form_elements/create_radio_buttons.dart';
 import '../form_elements/icon_chooser.dart';
 
-class CreateNewList extends StatelessWidget {
+class CreateNewList extends StatefulWidget {
+  final List ourList;
+  bool firstPart = false;
+
   final Map<String, dynamic> formData = {
     'id': null,
     'shareId': null,
     'title': null,
     'icon': 57744,
     'creator': 'Rider',
-    'permissions': null,
+    'fullPermissions': false,
     'items': {
       'incomplete': [],
       'complete': [{}]
     }
   };
-  static GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final List ourList;
+
   CreateNewList(this.ourList);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CreateNewList();
+  }
+}
+
+class _CreateNewList extends State<CreateNewList> {
+  static GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _returnText(wording) {
+    return Text(wording);
+  }
+
+  Widget _returnTitleAndPermissions(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _returnText('Enter a title'),
+        TextFormField(
+          onSaved: (String value) {
+            widget.formData['title'] = value;
+          },
+          validator: (String value) {
+            if (value.isEmpty) {
+              return 'A title is required';
+            }
+          },
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        _returnText('Choose list permissions'),
+        Container(
+          child: SwitchListTile(
+            value: widget.formData['fullPermissions'],
+            onChanged: (bool value) {
+              setState(() {
+                widget.formData['fullPermissions'] = value;
+              });
+            },
+            title: widget.formData['fullPermissions']
+                ? Text('Full permissions')
+                : Text('Limited Permissions'),
+          ),
+        ),
+        Container(
+          child: widget.formData['fullPermissions']
+              ? Text('Anyone can edit, delete, and invite people to this list')
+              : Text(
+                  'Permissions are limited to the creator of the list. Users can still be invited and complete tasks.'),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        RaisedButton(
+          textColor: Colors.white,
+          color: Colors.blue,
+          child: Text('Next'),
+          onPressed: () {
+            _formKey.currentState.save();
+            if (!_formKey.currentState.validate() ||
+                widget.formData['permissions'] == null) {
+              return;
+            }
+            setState(() {
+              widget.firstPart = true;
+            });
+            // await putting into db
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _returnIconChooseArea(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _returnText('Choose an icon'),
+        Flexible(
+          fit: FlexFit.loose,
+          child: IconChooser(widget.formData),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        RaisedButton(
+          textColor: Colors.white,
+          color: Colors.blue,
+          child: Text('Create'),
+          onPressed: () {
+            _formKey.currentState.save();
+            if (!_formKey.currentState.validate() ||
+                widget.formData['permissions'] == null) {
+              return;
+            }
+            final Uuid uuid = new Uuid();
+            final String newID = uuid.v1();
+            widget.formData['id'] = newID;
+            widget.formData['shareId'] = newID.split('-')[0];
+            widget.ourList.add(widget.formData);
+            // await putting into db
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,61 +140,13 @@ class CreateNewList extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(20.0),
         child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text('Enter a title'),
-              TextFormField(
-                onSaved: (String value) {
-                  formData['title'] = value;
-                },
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'A title is required';
-                  }
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('Choose list permissions'),
-              SizedBox(
-                height: 10,
-              ),
-              CreateRadioButtons(formData),
-              SizedBox(
-                height: 10,
-              ),
-              Flexible(
-                fit: FlexFit.loose,
-                child: IconChooser(formData),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              RaisedButton(
-                textColor: Colors.white,
-                color: Colors.blue,
-                child: Text('Create'),
-                onPressed: () {
-                  _formKey.currentState.save();
-                  if (!_formKey.currentState.validate() ||
-                      formData['permissions'] == null) {
-                    return;
-                  }
-                  final Uuid uuid = new Uuid();
-                  final String newID = uuid.v1();
-                  formData['id'] = newID;
-                  formData['shareId'] = newID.split('-')[0];
-                  ourList.add(formData);
-                  // await putting into db
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          ),
-        ),
+            key: _formKey,
+            child: !widget.firstPart
+                ? _returnTitleAndPermissions(context)
+                : _returnIconChooseArea(context)),
+      ),
+      bottomSheet: LinearProgressIndicator(
+        value: !widget.firstPart ? 0.5 : 1.0,
       ),
     );
   }
