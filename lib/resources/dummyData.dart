261eab5914ea15292042ce5759e15053ebe33eff
Rider <riderjensen@gmail.com>
2019-01-05 18:29:37 -0700

    Updating dummy data for new model method

--- lib/resources/dummyData.dart
+++ lib/resources/dummyData.dart
@@ -1,12 +1,151 @@
-final List<Map> ourList = [
-  {
-    'id': 'c750f160-0b50-11e9-f50d-11e28813cc33',
-    'shareId': 'c750f160',
-    'title': 'My first list',
-    'icon': 0xe192,
-    'creator': 'Rider',
-    'permissions': 'full',
-    'items': {
+import '../models/list_model.dart';
+
+final ListModel pracModel3 = new ListModel(
+    id: 'c11777b0-0b50-11e9-fa15-4781cd3d8c2f',
+    shareId: 'c11777b0',
+    title: 'My second list',
+    icon: 0xe193,
+    creator: 'Rider',
+    fullPermissions: false,
+    items: {
+      'incomplete': [
+        'Here is an undone item on list two',
+        'here is the second undone item on list two',
+        'here is the thrid undone item on list two'
+      ],
+      'complete': [
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the first item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the second item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        },
+        {
+          'item': 'here is the third item on list two',
+          'date': 'date here',
+          'userCom': 'Rider'
+        }
+      ]
+    });
+
+final ListModel pracModel2 = new ListModel(
+    id: 'cba0c380-0b50-11e9-9048-3fc21acebc18',
+    shareId: 'cba0c380',
+    title: 'My second list',
+    icon: 0xe191,
+    creator: 'Rider',
+    fullPermissions: true,
+    items: {
+      'incomplete': [
+        'Here is an undone item on list two',
+        'here is the second undone item on list two',
+        'here is the thrid undone item on list two'
+      ],
+      'complete': [{}]
+    });
+
+final ListModel pracModel1 = new ListModel(
+    id: 'c750f160-0b50-11e9-f50d-11e28813cc33',
+    shareId: 'c750f160',
+    title: 'My first list',
+    icon: 0xe192,
+    creator: 'Rider',
+    fullPermissions: true,
+    items: {
       'incomplete': [
         'Here is an undone item',
         'here is the second undone item',
@@ -29,144 +168,6 @@ final List<Map> ourList = [
           'userCom': 'Rider'
         }
       ]
-    }
-  },
-  {
-    'id': 'cba0c380-0b50-11e9-9048-3fc21acebc18',
-    'shareId': 'cba0c380',
-    'title': 'My second list',
-    'icon': 0xe191,
-    'creator': 'Rider',
-    'permissions': 'full',
-    'items': {
-      'incomplete': [
-        'Here is an undone item on list two',
-        'here is the second undone item on list two',
-        'here is the thrid undone item on list two'
-      ],
-      'complete': [{}]
-    }
-  },
-  {
-    'id': 'c11777b0-0b50-11e9-fa15-4781cd3d8c2f',
-    'shareId': 'c11777b0',
-    'title': 'My second list',
-    'icon': 0xe193,
-    'creator': 'Rider',
-    'permissions': 'full',
-    'items': {
-      'incomplete': [
-        'Here is an undone item on list two',
-        'here is the second undone item on list two',
-        'here is the thrid undone item on list two'
-      ],
-      'complete': [
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the first item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the second item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        },
-        {
-          'item': 'here is the third item on list two',
-          'date': 'date here',
-          'userCom': 'Rider'
-        }
-      ]
-    }
-  },
-];
+    });
+
+final List<ListModel> ourList = [pracModel1, pracModel2, pracModel3];
