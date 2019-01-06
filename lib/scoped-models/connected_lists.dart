95dec447b65d5f0bd93e81d73120550991c25c78
Unknown <riderjensen@gmail.com>
2019-01-06 13:17:00 -0700

    Adding some getters and setters

--- lib/scoped-models/connected_lists.dart
+++ lib/scoped-models/connected_lists.dart
@@ -1,22 +1,21 @@
 import 'package:scoped_model/scoped_model.dart';
+
 import '../models/list_model.dart';
 import '../models/user.dart';
-import '../resources/dummyUser.dart';
 import '../resources/dummyData.dart';
 
 mixin ConnectedLists on Model {
   List<ListModel> _userLists = [];
-  UserModel _authenticatedUser;
-  String _selectedListNumber;
+  UserModel _authenticatedUser = new UserModel(
+    username: 'Riderjensen',
+    email: 'riderjensen@gmail.com',
+    lists: ['c750f160', 'cba0c380'],
+  );
+  String _selectedListCode;
 }
 
 mixin GetListInformation on ConnectedLists {
   List<ListModel> get userLists {
-    // query db for each firstUser.lists
-    firstUser.lists.forEach((listId) {
-      _userLists
-          .add(ourList[ourList.indexWhere((item) => item.shareId == listId)]);
-    });
     return _userLists;
   }
 
@@ -25,6 +24,31 @@ mixin GetListInformation on ConnectedLists {
   }
 
   String get selectedItem {
-    return _selectedListNumber;
+    return _selectedListCode;
+  }
+
+  ListModel get getOneList {
+    return _userLists[
+        _userLists.indexWhere((item) => item.shareId == _selectedListCode)];
+  }
+
+  void addToUserLists(ListModel incomingListAddition) {
+    _userLists.add(incomingListAddition);
+  }
+
+  void selectAListCode(String code) {
+    _selectedListCode = code;
+  }
+
+  void setUserLists() {
+    // query db for each firstUser.lists
+    _authenticatedUser.lists.forEach((listId) {
+      _userLists
+          .add(ourList[ourList.indexWhere((item) => item.shareId == listId)]);
+    });
+  }
+
+  void addANewList(String code) {
+    _authenticatedUser.lists.add(code);
   }
 }
