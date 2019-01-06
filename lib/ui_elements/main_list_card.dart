32750077d360061d66eb03a05974552b673ba0f1
Rider <riderjensen@gmail.com>
2019-01-05 22:57:06 -0700

    Adding toggle on the card to delete

--- lib/ui_elements/main_list_card.dart
+++ lib/ui_elements/main_list_card.dart
@@ -5,6 +5,7 @@ import '../models/user.dart';
 class MainListCard extends StatefulWidget {
   final List<ListModel> ourList;
   final UserModel ourUser;
+
   MainListCard(this.ourList, this.ourUser);
 
   @override
@@ -16,23 +17,59 @@ class MainListCard extends StatefulWidget {
 class _MainListCard extends State<MainListCard> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       new GlobalKey<RefreshIndicatorState>();
+  bool ourCardDelete = false;
+
+  Widget _oneSideCard(int) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+            leading: Icon(
+              IconData(widget.ourList[int].icon, fontFamily: 'MaterialIcons'),
+            ),
+            title: Text(widget.ourList[int].title),
+            subtitle: Text('Creator: ${widget.ourList[int].creator}'),
+            onTap: () {
+              Navigator.pushNamed<dynamic>(context, '/list/' + int.toString());
+            }),
+      ],
+    );
+  }
+
+  Widget _deleteSideCard(int) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(color: Color.fromRGBO(255, 0, 0, 0.3)),
+          child: ListTile(
+            leading: IconButton(
+              color: Colors.red,
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // remove this item from the list
+              },
+            ),
+            title: Text(widget.ourList[int].title),
+            subtitle: Text('Creator: ${widget.ourList[int].creator}'),
+          ),
+        )
+      ],
+    );
+  }
+
   Widget returnCard(int) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-                leading: Icon(IconData(widget.ourList[int].icon,
-                    fontFamily: 'MaterialIcons')),
-                title: Text(widget.ourList[int].title),
-                subtitle: Text('Creator: ${widget.ourList[int].creator}'),
-                onTap: () {
-                  Navigator.pushNamed<dynamic>(
-                      context, '/list/' + int.toString());
-                }),
-          ],
-        ),
+    return GestureDetector(
+      onLongPress: () {
+        setState(() {
+          widget.ourList[int].toggleDelete = !widget.ourList[int].toggleDelete;
+        });
+      },
+      child: Center(
+        child: Card(
+            child: widget.ourList[int].toggleDelete
+                ? _deleteSideCard(int)
+                : _oneSideCard(int)),
       ),
     );
   }
