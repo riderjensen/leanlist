5642979899f1f7b2f74babd567ae7dbc22061e52
Rider <riderjensen@gmail.com>
2019-01-07 21:25:27 -0700

    Adding sign in and sign up methods

--- lib/scoped-models/connected_lists.dart
+++ lib/scoped-models/connected_lists.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../local_keys.dart';
 
 import '../models/list_model.dart';
 import '../models/user.dart';
@@ -28,8 +34,23 @@ mixin GetListInformation on ConnectedLists {
         _userLists.indexWhere((item) => item.shareId == _selectedListCode)];
   }
 
-  void addToUserLists(ListModel incomingListAddition) {
-    _userLists.add(incomingListAddition);
+  void addToUserLists(ListModel incomingListAddition) async {
+    final Map<String, dynamic> myAddData = {
+      'id': incomingListAddition.id,
+      'shareId': incomingListAddition.shareId,
+      'title': incomingListAddition.title,
+      'icon': incomingListAddition.icon,
+      'creator': incomingListAddition.creator,
+      'fullPermissions': incomingListAddition.fullPermissions,
+      'items': [],
+      'toggleDelete': false
+    };
+    http
+        .post('https://lean-list.firebaseio.com/lists.json',
+            body: json.encode(myAddData))
+        .then((_) {
+      _userLists.add(incomingListAddition);
+    });
   }
 
   void selectAListCode(String code) {
@@ -37,7 +58,6 @@ mixin GetListInformation on ConnectedLists {
   }
 
   void setUserLists() {
-    // query db for each firstUser.lists
     _authenticatedUser.lists.forEach((listId) {
       _userLists
           .add(ourList[ourList.indexWhere((item) => item.shareId == listId)]);
@@ -52,23 +72,92 @@ mixin GetListInformation on ConnectedLists {
     _authenticatedUser.lists.add(code);
   }
 
-  void signIn(String username, String password) {
-    // check the user against the DB
-    _authenticatedUser = new UserModel(
-      username: username,
-      email: 'riderjensen@gmail.com',
-      lists: ['c750f160', 'cba0c380'],
+  Future<Map<String, dynamic>> signIn(String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
+    final http.Response userResponse = await http.post(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${firebaseKey}',
+      body: jsonEncode(authData),
+      headers: {'Content-Type': 'application/json'},
     );
-    setUserLists();
+
+    final Map<String, dynamic> theUserResponseDecoded =
+        jsonDecode(userResponse.body);
+    if (theUserResponseDecoded.containsKey('error')) {
+      return {
+        'success': false,
+        'message': theUserResponseDecoded['error']['message']
+      };
+    } else {
+      final http.Response fullUsersResponse =
+          await http.get('https://lean-list.firebaseio.com/users.json');
+
+      final Map<String, dynamic> allUsers = json.decode(fullUsersResponse.body);
+      allUsers.forEach((String id, dynamic listData) {
+        print(listData);
+        if (email == listData['email']) {
+          _authenticatedUser = new UserModel(
+            username: listData['username'],
+            email: listData['email'],
+            lists: listData['lists'] == null ? [] : listData['lists'],
+          );
+        } else {
+          print('missed');
+        }
+      });
+      setUserLists();
+      return {'success': true};
+    }
   }
 
-  void signUp(String username, String email, String password) {
-    // put in db
-    _authenticatedUser = new UserModel(
-      username: username,
-      email: email,
-      lists: ['c750f160', 'cba0c380'],
+  Future<Map<String, dynamic>> signUp(
+      String username, String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
+    final http.Response response = await http.post(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${firebaseKey}',
+      body: jsonEncode(authData),
+      headers: {'Content-Type': 'application/json'},
     );
-    setUserLists();
+
+    final Map<String, dynamic> theResponseDecoded = jsonDecode(response.body);
+
+    if (theResponseDecoded.containsKey('error')) {
+      return {
+        'success': false,
+        'message': theResponseDecoded['error']['message']
+      };
+    } else {
+      final Map<String, dynamic> myAddUser = {
+        'username': username,
+        'email': email,
+        'lists': []
+      };
+      final http.Response secondResponse = await http.post(
+          'https://lean-list.firebaseio.com/users.json',
+          body: json.encode(myAddUser));
+      final Map<String, dynamic> secondResponseDecoded =
+          jsonDecode(secondResponse.body);
+      if (secondResponseDecoded.containsKey('error')) {
+        return {
+          'success': false,
+          'message': theResponseDecoded['error']['message']
+        };
+      } else {
+        _authenticatedUser = new UserModel(
+          username: username,
+          email: email,
+          lists: [],
+        );
+        setUserLists();
+        return {'success': true};
+      }
+    }
   }
 }
